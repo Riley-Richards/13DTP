cc3db133ffab75a009a9d819a2c28cc38c091ed6
Riley-Richards <18204@burnside.school.nz>
2022-02-11 09:24:05 +1300

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import sqlalchemy
+from flask import Flask, render_template, request, redirect, abort
+
+
+app = Flask(__name__)
+
+
+# home page route
+@app.route('/')
+def home():
+    return render_template('home.html', title="Home")
+
+
+# 404 ERROR page route
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
+
+
+# about page route
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/gpus')
+def gpu():
+    return render_template('gpu.html')
+
+
+@app.route('/cpus')
+def cpu():
+    return render_template('cpu.html')
+
+
+@app.route('/motherboard')
+def mb():
+    return render_template('mb.html')
+
+
+@app.route('/memory')
+def memory():
+    return render_template('memory.html')
+
+
+@app.route('/storage')
+def storage():
+    return render_template('storage.html')
+
+
+@app.route('/psus')
+def psu():
+    return render_template('psu.html')
+
+
+@app.route('/case')
+def case():
+    return render_template('case.html')
+
+
+# runs port on local site
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
