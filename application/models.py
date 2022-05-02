79b8188b1dcf24f01be431f330c4ef91db27dfe5
Riley-Richards <18204@burnside.school.nz>
2022-05-02 12:15:09 +1200

    added customer model

--- application/models.py
+++ application/models.py
@@ -5,3 +5,11 @@ class Product(db.Model):
     name = db.Column(db.String(50), nullable=False, unique=True)
     price = db.Column(db.Integer, nullable=False)
     category = db.Column(db.String, nullable=False)
+
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    
