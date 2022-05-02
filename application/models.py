0081a987eebd2c7685a02780f9a412d6d0eecee6
Riley-Richards <18204@burnside.school.nz>
2022-05-02 16:57:26 +1200

    completed login/register and created db

--- application/models.py
+++ application/models.py
@@ -1,9 +1,10 @@
 from application import db, login_manager
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @login_manager.user_loader
 def load_user(user):
-    return User.get(user)
+    return User.query.get(user)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,11 +13,18 @@ class Product(db.Model):
     category = db.Column(db.String, nullable=False)
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User{}>'.format(self.email)
     
