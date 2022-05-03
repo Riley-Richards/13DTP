75bb8b4c3f9fac73dc77918461eeda95e04d540f
Riley-Richards <18204@burnside.school.nz>
2022-05-03 12:38:30 +1200

    added images and data to the database

--- application/models.py
+++ application/models.py
@@ -6,15 +6,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 def load_user(user):
     return User.query.get(user)
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False, unique=True)
-    price = db.Column(db.Integer, nullable=False)
-    category = db.Column(db.String, nullable=False)
-
 
 class User(db.Model, UserMixin):
-    __tablename__ = "User"
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
@@ -28,3 +22,17 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return '<User{}>'.format(self.email)
     
+
+class Product(db.Model):
+    __tablename__ = "product"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    info = db.Column(db.Text, nullable=False)
+
+
+class Category(db.Model):
+   __tablename__ = "category"
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(50), nullable=False, unique=True)
+   info = db.Column(db.Text, nullable=False)
